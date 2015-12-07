65f8da8d628bc9261eaeab655e8a826c22626847
Zareen Khayrattee <zareenk94@yahoo.com>
2015-12-06 20:21:24 -0600

    added other half of my bar chart to shiny...don't know how to make one chart with 2 row pills like in tableau

--- 03 Shiny/BarChart/server.R
+++ 03 Shiny/BarChart/server.R
@@ -10,9 +10,7 @@ require("shinydashboard")
 
 shinyServer(function(input, output) {
   
-#  df2 <- eventReactive(input$clicks2, {df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select GBDCHILDCAUSES, country, sum(sum_deaths_0_to_27_days) as sum_deaths_0_to_27_days, avg_deaths_0_to_27_days, avg(avg_deaths_0_to_27_days) OVER (PARTITION BY gbdchildcauses) as window_avg from (select country, GBDCHILDCAUSES, sum(deaths_0_to_27_days) as sum_deaths_0_to_27_days, avg(deaths_0_to_27_days) as avg_deaths_0_to_27_days from child_deaths where (country = \'Dominican Republic\' or country = \'Guatemala\' or country = \'Haiti\' or country = \'Mexico\' or country = \'USA\') and (gbdchildcauses = \'Acute lower respiratory infections\' or  gbdchildcauses = \'Congenital anomalies\' or gbdchildcauses = \'Injuries\' or gbdchildcauses = \'Other communicable, perinatal and nutritional conditions\' or gbdchildcauses = \'Prematurity\') group by country, gbdchildcauses) group by country, gbdchildcauses, avg_deaths_0_to_27_days;"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_znk74', PASS='orcl_znk74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))})
-  
-  df2 <- eventReactive(input$clicks2, {df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select GBDCHILDCAUSES, country, sum(sum_deaths_0_to_27_days) as sum_deaths_0_to_27_days, avg(sum_deaths_0_to_27_days) OVER (PARTITION BY gbdchildcauses) as window_avg from (select country, GBDCHILDCAUSES, sum(deaths_0_to_27_days) as sum_deaths_0_to_27_days, avg(deaths_0_to_27_days) as avg_deaths_0_to_27_days from child_deaths where (country = \'Dominican Republic\' or country = \'Guatemala\' or country = \'Haiti\' or country = \'Mexico\' or country = \'USA\') and (gbdchildcauses = \'Acute lower respiratory infections\' or  gbdchildcauses = \'Congenital anomalies\' or gbdchildcauses = \'Injuries\' or gbdchildcauses = \'Other communicable, perinatal and nutritional conditions\' or gbdchildcauses = \'Prematurity\') group by country, gbdchildcauses) group by country, gbdchildcauses, sum_deaths_0_to_27_days;"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_znk74', PASS='orcl_znk74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))})
+  df1 <- eventReactive(input$clicks1, {df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select GBDCHILDCAUSES, country, sum(sum_deaths_0_to_27_days) as sum_deaths_0_to_27_days, avg(sum_deaths_0_to_27_days) OVER (PARTITION BY gbdchildcauses) as window_avg from (select country, GBDCHILDCAUSES, sum(deaths_0_to_27_days) as sum_deaths_0_to_27_days, avg(deaths_0_to_27_days) as avg_deaths_0_to_27_days from child_deaths where (country = \'Dominican Republic\' or country = \'Guatemala\' or country = \'Haiti\' or country = \'Mexico\' or country = \'USA\') and (gbdchildcauses = \'Acute lower respiratory infections\' or  gbdchildcauses = \'Congenital anomalies\' or gbdchildcauses = \'Injuries\' or gbdchildcauses = \'Other communicable, perinatal and nutritional conditions\' or gbdchildcauses = \'Prematurity\') group by country, gbdchildcauses) group by country, gbdchildcauses, sum_deaths_0_to_27_days;"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_znk74', PASS='orcl_znk74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))})
     
     #spread(df, YEAR, SUM_PERCENTAGE)
     
@@ -24,7 +22,7 @@ shinyServer(function(input, output) {
       facet_wrap(~GBDCHILDCAUSES) +
       labs(title='Child Deaths Barchart') +
       labs(x=paste("Cause of Death"), y=paste("Deaths")) +
-      layer(data=df2(), 
+      layer(data=df1(), 
             mapping=aes(x=COUNTRY, y=SUM_DEATHS_0_TO_27_DAYS, fill=COUNTRY), 
             stat="identity", 
             stat_params=list(), 
@@ -32,12 +30,12 @@ shinyServer(function(input, output) {
             geom_params=list(colour="blue"), 
             position=position_identity()
       ) +
-      layer(data=df2(), 
+      layer(data=df1(), 
             mapping=aes(yintercept = WINDOW_AVG), 
             geom="hline",
             geom_params=list(colour="red")
       ) + 
-      layer(data=df2(), 
+      layer(data=df1(), 
             mapping=aes(x=COUNTRY, y=SUM_DEATHS_0_TO_27_DAYS, label=SUM_DEATHS_0_TO_27_DAYS), 
             stat="identity", 
             stat_params=list(), 
@@ -46,7 +44,7 @@ shinyServer(function(input, output) {
             geom_params=list(colour="black", hjust=-0.5), 
             position=position_identity()
       ) +
-      layer(data=df2(), 
+      layer(data=df1(), 
             mapping=aes(x=COUNTRY, y=WINDOW_AVG, label=WINDOW_AVG), 
             stat="identity", 
             stat_params=list(), 
@@ -57,5 +55,53 @@ shinyServer(function(input, output) {
       ) 
       plot1
       })
+    
+    df2 <- eventReactive(input$clicks2, {df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select GBDCHILDCAUSES, country, sum(sum_deaths_1_to_59_months) as sum_deaths_1_to_59_months, avg(sum_deaths_1_to_59_months) OVER (PARTITION BY gbdchildcauses) as window_avg from (select country, GBDCHILDCAUSES, sum(deaths_1_to_59_months) as sum_deaths_1_to_59_months, avg(deaths_1_to_59_months) as avg_deaths_1_to_59_months from child_deaths where (country = \'Dominican Republic\' or country = \'Guatemala\' or country = \'Haiti\' or country = \'Mexico\' or country = \'USA\') and (gbdchildcauses = \'Acute lower respiratory infections\' or  gbdchildcauses = \'Congenital anomalies\' or gbdchildcauses = \'Injuries\' or gbdchildcauses = \'Other communicable, perinatal and nutritional conditions\' or gbdchildcauses = \'Prematurity\') group by country, gbdchildcauses) group by country, gbdchildcauses, sum_deaths_1_to_59_months;"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_znk74', PASS='orcl_znk74', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))})
+    
+    #spread(df, YEAR, SUM_PERCENTAGE)
+    
+    output$barchart2 <- renderPlot(height=800, width=1500, {
+      plot2 <- ggplot() + 
+        coord_cartesian() + 
+        scale_x_discrete() +
+        scale_y_continuous() +
+        facet_wrap(~GBDCHILDCAUSES) +
+        labs(title='Child Deaths Barchart') +
+        labs(x=paste("Cause of Death"), y=paste("Deaths")) +
+        layer(data=df2(), 
+              mapping=aes(x=COUNTRY, y=SUM_DEATHS_1_TO_59_MONTHS, fill=COUNTRY),               stat="identity", 
+              stat_params=list(), 
+              geom="bar",
+              geom_params=list(colour="blue"), 
+              position=position_identity()
+        ) +
+        layer(data=df2(), 
+              mapping=aes(yintercept = WINDOW_AVG), 
+              geom="hline",
+              geom_params=list(colour="red")
+        ) + 
+        layer(data=df2(), 
+              mapping=aes(x=COUNTRY, y=SUM_DEATHS_1_TO_59_MONTHS, label=SUM_DEATHS_1_TO_59_MONTHS), 
+              stat="identity", 
+              stat_params=list(), 
+              geom="text",
+              label=value,
+              geom_params=list(colour="black", hjust=-0.5), 
+              position=position_identity()
+        ) +
+        layer(data=df2(), 
+              mapping=aes(x=COUNTRY, y=WINDOW_AVG, label=WINDOW_AVG), 
+              stat="identity", 
+              stat_params=list(), 
+              geom="text",
+              label=value,
+              geom_params=list(colour="red", hjust=-5), 
+              position=position_identity()
+        ) 
+      plot2
+    })
+    
+    
+    
 }
 )
--- 03 Shiny/BarChart/ui.R
+++ 03 Shiny/BarChart/ui.R
@@ -7,12 +7,18 @@ library(shiny)
 
 navbarPage(
   title = "DV_FinalProject",
-  tabPanel(title= "Barchart",
+  tabPanel(title= "Barchart: 0-27 Days",
            sidebarPanel(
-             actionButton(inputId="clicks2", label = "Click me")
+             actionButton(inputId="clicks1", label = "Show Barchart: 0-27 Days")
            ),
            mainPanel(plotOutput("barchart"))
-          )
+          ), 
+  tabPanel(title= "Barchart: 1-59 Months",
+           sidebarPanel(
+             actionButton(inputId="clicks2", label = "Show Barchart: 1-59 Months")
+           ),
+           mainPanel(plotOutput("barchart2"))
+  )
            )
 #))
 #fluidPage(
